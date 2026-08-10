f234c898477ec57645bf7230f0de9e368c2c66b1
Gabriel Benitez <ezebenitez8006@gmail.comgit config --global user.name Gabriel>
2026-08-10 14:41:39 -0300

    Actualizo archivo SQL con modificaciones

--- sistema_museon.sql
+++ sistema_museon.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 10-08-2026 a las 19:37:41
+-- Tiempo de generación: 10-08-2026 a las 19:41:02
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `sistema_objetos`
+-- Base de datos: `sistema_museon`
 --
 
 -- --------------------------------------------------------
